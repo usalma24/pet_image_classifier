1c591b8fd07ea04f792e0ddfa7bd258d519ea3dc
Umme salma <114981000+usalma24@users.noreply.github.com>
2024-11-22 06:56:02 -0800

    Update check_images.py

--- image_classifier/workspace/cd0184/9664b117-d773-4799-b6a3-d4640ed70218/data/check_images.py
+++ image_classifier/workspace/cd0184/9664b117-d773-4799-b6a3-d4640ed70218/data/check_images.py
@@ -39,10 +39,10 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # TODO 0: Measures total program runtime by collecting start time
+    # Measures total program runtime by collecting start time
     start_time = time()
     
-    # TODO 1: Define get_input_args function within the file get_input_args.py
+    # Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
     # the user running the program from a terminal window. This function returns
     # the collection of these command line arguments from the function call as
@@ -55,7 +55,7 @@ def main():
     check_command_line_arguments(in_arg)
 
     
-    # TODO 2: Define get_pet_labels function within the file get_pet_labels.py
+    #  Define get_pet_labels function within the file get_pet_labels.py
     # Once the get_pet_labels function has been defined replace 'None' 
     # in the function call with in_arg.dir  Once you have done the replacements
     # your function call should look like this: 
@@ -70,7 +70,7 @@ def main():
     check_creating_pet_image_labels(results)
 
 
-    # TODO 3: Define classify_images function within the file classiy_images.py
+    # Define classify_images function within the file classiy_images.py
     # Once the classify_images function has been defined replace first 'None' 
     # in the function call with in_arg.dir and replace the last 'None' in the
     # function call with in_arg.arch  Once you have done the replacements your
@@ -123,10 +123,10 @@ def main():
     #print results
     print_results(results, results_stats, in_arg.arch, True, True)
     
-    # TODO 0: Measure total program runtime by collecting end time
+    # Measure total program runtime by collecting end time
     end_time = time()
     
-    # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
+    # Computes overall runtime in seconds & prints it in hh:mm:ss format
     tot_time = end_time - start_time
     print("\n** Total Elapsed Runtime:", str(int(tot_time / 3600)) + ":" + str(int((tot_time % 3600) / 60)) + ":" + str(int((tot_time % 3600) % 60)))
     
